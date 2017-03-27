84a097e4a7e90ab734ea07a23cb20af448c46bfb
Bogdan Nechita <talk.bogdan@gmail.com>
2017-03-27 16:28:35 +0300

    Add UITest onePlusOne.

--- RomanNumeralCalculatorUITests/RomanNumeralCalculatorUITests.swift
+++ RomanNumeralCalculatorUITests/RomanNumeralCalculatorUITests.swift
@@ -28,9 +28,18 @@ class RomanNumeralCalculatorUITests: XCTestCase {
         super.tearDown()
     }
     
-    func testExample() {
-        // Use recording to get started writing UI tests.
-        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    func testOnePlusOne() {
+        
+        let app = XCUIApplication()
+        let iButton = app.buttons["I"]
+        let calculatedLabel = app.staticTexts["calculated_value"]
+        
+        iButton.tap()
+        XCTAssertEqual(calculatedLabel.label, "I")
+        app.buttons["+"].tap()
+        iButton.tap()
+        XCTAssertEqual(calculatedLabel.label, "II")
+        
     }
     
 }
